b2eacbeb60ee028af72693e1efe7ad79e3fa4cd1
Karl Mutch <karlmutch@gmail.com>
2018-02-16 06:51:51 -0800

    fixes for underscores in branch names

--- cmd/bump-ver/build.sh
+++ cmd/bump-ver/build.sh
@@ -14,16 +14,35 @@ export DATE=`date '+%Y-%m-%d_%H:%M:%S%z'`
 export PATH=$PATH:$GOPATH/bin
 go get -u -f github.com/golang/dep/cmd/dep
 go get -u -f github.com/aktau/github-release
+export SEMVER=`bump-ver -f ./README.md extract`
+TAG_PARTS=$(echo $SEMVER | sed "s/-/\n-/g" | sed "s/\./\n\./g" | sed "s/+/\n+/g")
+PRE_RELEASE=""
+for part in $TAG_PARTS
+do
+    start=`echo "$part" | cut -c1-1`
+    if [ "$start" == "+" ]; then
+        break
+    fi
+    if [ "$start" == "-" ]; then
+        PRE_RELEASE+=$part
+    fi
+done
+flags='-X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH -X github.com/karlmutch/bump-ver/version.SemVer="${SEMVER}"'
+flags="$(eval echo $flags)"
 dep ensure -no-vendor
 [ -e vendor/github.com/karlmutch/bump-ver ] || mkdir -p vendor/github.com/karlmutch/bump-ver
 mkdir -p cmd/bump-ver/bin
-CGO_ENABLED=0 go build -ldflags "-X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH" -o cmd/bump-ver/bin/bump-ver cmd/bump-ver/*.go
+CGO_ENABLED=0 go build -ldflags "$flags" -o cmd/bump-ver/bin/bump-ver cmd/bump-ver/*.go
 cp cmd/bump-ver/bin/bump-ver $GOPATH/bin/.
-go build -ldflags "-X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH" -race -o cmd/bump-ver/bin/bump-ver-race cmd/bump-ver/*.go
-CGO_ENABLED=0 go test -ldflags "-X github.com/karlmutch/bump-ver/version.TestRunMain=Use -X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH" -coverpkg="./..." -c -o cmd/bump-ver/bin/bump-ver-run-coverage cmd/bump-ver/*.go
-CGO_ENABLED=0 go test -ldflags "-X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH" -coverpkg="./..." -c -o cmd/bump-ver/bin/bump-ver-test-coverage cmd/bump-ver/*.go
-go test -ldflags "-X github.com/karlmutch/bump-ver/version.BuildTime=$DATE -X github.com/karlmutch/bump-ver/version.GitHash=$HASH" -race -c -o cmd/bump-ver/bin/bump-ver-test cmd/bump-ver/*.go
-if ! [ -z "${GITHUB_TOKEN}" ]; then
-    github-release release --user karlmutch --repo bump-ver --tag ${TRAVIS_TAG} --pre-release && \
-    github-release upload --user karlmutch --repo bump-ver  --tag ${TRAVIS_TAG} --name bump-ver --file cmd/bump-ver/bin/bump-ver
+go build -ldflags "$flags" -race -o cmd/bump-ver/bin/bump-ver-race cmd/bump-ver/*.go
+CGO_ENABLED=0 go test -ldflags "$flags" -coverpkg="./..." -c -o cmd/bump-ver/bin/bump-ver-run-coverage cmd/bump-ver/*.go
+CGO_ENABLED=0 go test -ldflags "$flags" -coverpkg="./..." -c -o cmd/bump-ver/bin/bump-ver-test-coverage cmd/bump-ver/*.go
+go test -ldflags "$flags" -race -c -o cmd/bump-ver/bin/bump-ver-test cmd/bump-ver/*.go
+if [ -z "$PRE_RELEASE" ]; then
+    if ! [ -z "${SEMVER}" ]; then
+        if ! [ -z "${GITHUB_TOKEN}" ]; then
+            github-release release --user karlmutch --repo bump-ver --tag ${SEMVER} --pre-release && \
+            github-release upload --user karlmutch --repo bump-ver  --tag ${SEMVER} --name bump-ver --file cmd/bump-ver/bin/bump-ver
+        fi
+    fi
 fi
