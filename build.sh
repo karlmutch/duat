0497f07def9443bf446163ee4e4c3fbd25f2ec07
Karl Mutch <karlmutch@gmail.com>
2019-04-04 20:16:18 -0700

    Use the workstation dockerfile by default in the build.sh script for localized releases

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker build -t duat-build:latest --build-arg USER=$USER --build-arg USER_ID=`id -u $USER` --build-arg USER_GROUP_ID=`id -g $USER` .
+docker build -f Dockerfile_workstation -t duat-build:latest --build-arg USER=$USER --build-arg USER_ID=`id -u $USER` --build-arg USER_GROUP_ID=`id -g $USER` .
 docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -v $GOPATH:/project duat-build:latest
 result=$?
 if [ $result -ne 0 ]; then
